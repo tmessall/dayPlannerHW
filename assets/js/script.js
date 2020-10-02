f135bfe4f4c2e8dbf852d3c759ef9393cc1f0d94
tmessall <tmessall17@gmail.com>
2020-10-02 11:52:26 -0600

    Added array to keep track of events so they can be added to local storage (yet to add local storage code)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function (){
     var planner = $(".container");
 
+    // Keeps track of every event
+    var events = [];
+
+    // Loops through, creates each row
     for (var i = 9; i < 18; i++) {
         var row = $("<div>");
         row.addClass("row time-block");
@@ -8,12 +12,14 @@ $(document).ready(function (){
         hour.addClass("hour");
         hour.width("5%");
         var event = $("<textarea>");
+        // Needs to include check for if past, present, future, add class
         event.addClass("textarea");
         event.width("80%");
         var saveBtn = $("<button>");
         saveBtn.text("lock");
         saveBtn.addClass("saveBtn");
         saveBtn.width("5%");
+        // Adds a time to each hour
         if (i < 12) {
             hour.text(i + " AM");
         } else if (i === 12) {
@@ -21,12 +27,24 @@ $(document).ready(function (){
         } else {
             hour.text((i-12) + " PM");
         }
-        event.attr("value", i);
-        saveBtn.attr("value", i);
+        // Values are saved so that each event can be saved and accessed
+        event.attr("time", i);
+        events.push(event);
+        saveBtn.attr("time", i);
+        // Makes the row
         row.append(hour);
         row.append(event);
         row.append(saveBtn);
         planner.append(row);
-    } 
+    }
+    
+    $(".saveBtn").click(function() {
+        var time = $(this).attr("time");
+        for (var i = 0; i < events.length; i++) {
+            if (events[i].attr("time") === time) {
+                console.log(events[i].val());
+            }
+        }
+    })
 })
 
