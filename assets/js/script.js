7c2c8d8bde88c1b5fe13867c7ab44d3578e77285
tmessall <tmessall17@gmail.com>
2020-10-02 12:28:50 -0600

    Added the local storage capability

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function (){
     var events = [];
 
     // Loops through, creates each row
-    for (var i = 9; i < 18; i++) {
+    for (var i = 0; i < 9; i++) {
         var row = $("<div>");
         row.addClass("row time-block");
         var hour = $("<div>");
@@ -16,20 +16,20 @@ $(document).ready(function (){
         event.addClass("textarea");
         event.width("80%");
         var saveBtn = $("<button>");
-        saveBtn.text("lock");
+        saveBtn.text("save");
         saveBtn.addClass("saveBtn");
         saveBtn.width("5%");
         // Adds a time to each hour
-        if (i < 12) {
-            hour.text(i + " AM");
-        } else if (i === 12) {
-            hour.text(i + " PM");
+        if (i < 3) {
+            hour.text((i + 9) + " AM");
+        } else if (i === 3) {
+            hour.text((i + 9) + " PM");
         } else {
-            hour.text((i-12) + " PM");
+            hour.text((i-3) + " PM");
         }
         // Values are saved so that each event can be saved and accessed
         event.attr("time", i);
-        events.push(event);
+        events[i] = event;
         saveBtn.attr("time", i);
         // Makes the row
         row.append(hour);
@@ -37,12 +37,30 @@ $(document).ready(function (){
         row.append(saveBtn);
         planner.append(row);
     }
+
+    // Sets the event for a time to the locally saved event
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            var localEvent = localStorage.getItem(j);
+            if (localEvent !== null) {
+                if (i === j) {
+                    events[i].val(localEvent);
+                    console.log(i);
+                    console.log(j);
+                    console.log(typeof localEvent); 
+                }
+                
+            }
+        }
+    }
     
+    // Saves event to local storage
     $(".saveBtn").click(function() {
         var time = $(this).attr("time");
         for (var i = 0; i < events.length; i++) {
             if (events[i].attr("time") === time) {
-                console.log(events[i].val());
+                console.log("hey i work")
+                localStorage.setItem(i, events[i].val());
             }
         }
     })
